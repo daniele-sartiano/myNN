4e61268d97b8aa9cc9a49c5b11ed19f4414ae8cf
dsartiano <Tungsteno236270>
2019-04-11 21:01:17 +0200

    Updated python3 support

--- myNN.py
+++ myNN.py
@@ -7,17 +7,23 @@ class MyNN(object):
         self.seed = seed
         self.weights = 2 * np.random.random((3, 1)) -1
 
+    def sigmoid(self, x):
+        """ Normalize between 0 and 1"""
+        return 1 / (1 + np.exp(-x))
+
+    def sigmoid_derivative(self, x):
+        """ Gradient of the sigmoid curve: how confident we are about the existing weights."""
+        return x * (1-x)
+        
     def train(self, X, y, epochs):
-        for epoch in xrange(epochs):
+        for epoch in range(epochs):
             output = self.predict(X)
             error = y - output
-            adj = np.dot(X.T, error * (output * (1-output)))
+            adj = np.dot(X.T, error * self.sigmoid_derivative(output))
             self.weights += adj
-            # if (epoch % 1000 == 0):
-            #     print "Error", error
 
     def predict(self, X):
-        return 1/(1+np.exp(-np.dot(X, self.weights)))
+        return self.sigmoid(np.dot(X, self.weights))
 
 epochs = int(sys.argv[1])
 
@@ -28,4 +34,6 @@ y = np.array([[0,1,1,0]]).T
 
 nn.train(X, y, epochs)
 
-print nn.predict([1,0,1])
+print(nn.predict([1,0,1]))
+print(nn.predict([0,0,1]))
+print(nn.predict([1,1,1]))
